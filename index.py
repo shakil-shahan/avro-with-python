811a79688bd2aa7551329ec031c15e55e0e2a0db
Md. Shakil Mahmud Shahan <shakil.shahan@bs-23.net>
2021-02-23 18:56:23 +0600

    update index

--- index.py
+++ index.py
@@ -2,14 +2,32 @@ import avro.schema
 from avro.datafile import DataFileReader, DataFileWriter
 from avro.io import DatumReader, DatumWriter
 
-schema = avro.schema.parse(open("user.avsc", "rb").read())
+# import schema
 
-writer = DataFileWriter(open("users.avro", "wb"), DatumWriter(), schema)
+# from file
+# schema = avro.schema.parse(open("schema.avsc", "rb").read())
+
+# define here
+schema = avro.schema.parse("""{
+    "namespace": "example.avro",
+    "type": "record",
+    "name": "User",
+    "fields": [
+            {"name": "name", "type": "string"},
+            {"name": "favorite_number", "type": ["int", "null"]},
+            {"name": "favorite_color", "type": ["string", "null"]}
+    ]
+}""")
+
+# write a file
+writer = DataFileWriter(
+    open("./data/results.avro", "wb"), DatumWriter(), schema)
 writer.append({"name": "Alyssa", "favorite_number": 256})
 writer.append({"name": "Ben", "favorite_number": 7, "favorite_color": "red"})
 writer.close()
 
-reader = DataFileReader(open("users.avro", "rb"), DatumReader())
-for user in reader:
-    print(user)
+# read a file
+reader = DataFileReader(open("./data/results.avro", "rb"), DatumReader())
+for data in reader:
+    print(data)
 reader.close()
